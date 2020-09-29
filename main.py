e0234e528d35964de858432e554dce4d747fdaec
dcorso21 <david.l.corson@gmail.com>
2020-09-29 14:12:07 -0400

    creation of ui in console

--- main.py
+++ main.py
@@ -1,32 +1,47 @@
 # Data from https://gist.github.com/jaidevd/23aef12e9bf56c618c41
 import sqlite3
+from sqlite3.dbapi2 import ProgrammingError
 import pandas as pd
-import os
+# import os
+
 
 lib_name = "inHouse.db"
 withdrawn_name = "withdrawals.db"
 
 
+def tab_df(df):
+    from tabulate import tabulate
+    import json
+    if type(df) == dict:
+        df = json.dumps(df, indent=2)
+        print(df)
+        return
+    tablefmt = 'fancy_grid'
+    print(tabulate(df, headers=df.columns, tablefmt=tablefmt))
+
+
 def initialize_lib_db():
     lib_db = sqlite3.connect(lib_name)
     curs = lib_db.cursor()
-    # Init
-    # curs.execute(
-    #     """
-    # CREATE TABLE inHouse (
-    #     Title text,
-    #     Author text,
-    #     Genre text,
-    #     SubGenre text,
-    #     Pages integer,
-    #     Publisher text)
-    # """
-    # )
-    lib_db.commit()
+    try:
+        curs.execute(
+            """
+        CREATE TABLE inHouse (
+            Title text,
+            Author text,
+            Genre text,
+            SubGenre text,
+            Pages text,
+            Publisher text)
+        """
+        )
+        lib_db.commit()
+    except:
+        pass
     s = curs.fetchall()
     if len(s) != 0:
         curs.execute("DELETE * FROM inHouse")
-    df = pd.read_csv("books.csv")
+    df = pd.read_csv("books.csv").head(15)
     df = df.rename(columns={"Height": "Pages"})
     for i in df.index:
         curs.execute(
@@ -40,7 +55,6 @@ def initialize_lib_db():
             df.iloc[i].to_dict(),
         )
     lib_db.commit()
-    # lib_db.close()
     return lib_db, curs
 
 
@@ -55,8 +69,17 @@ def initialize_withdrawals():
     return with_db, wcurs
 
 
-lib_db, curs = initialize_lib_db()
-with_db, wcurs = initialize_withdrawals()
+def display_books():
+    with lib_db:
+        curs.execute("""SELECT * FROM inHouse""")
+        books = curs.fetchall()
+    books = pd.DataFrame(
+        data=books,
+        columns=["Title", "Author", "Genre", "SubGenre", "Pages", "Publisher"],
+    )
+    books['Pages'] = books.Pages.apply(lambda x: str(x).encode('utf-8'))
+    tab_df(books)
+    main_menu()
 
 
 def add_book(title, author, genre, subgenre, pages, publisher):
@@ -81,89 +104,144 @@ def add_book(title, author, genre, subgenre, pages, publisher):
         )
 
 
-def withdraw_book(title, return_date):
-    # global with_db, wcurs
-    with lib_db:
-        curs.execute(
-            """
-            SELECT * FROM inHouse WHERE Title = :title
-            """,
-            {"title": title},
-        )
-        book_info = curs.fetchone()
-    with with_db:
-        # if len(wcurs.fetchone()) != 1:
-        try:
-            wcurs.execute(
-            """
-            CREATE TABLE withdrawals (
-                Title text,
-                Author text,
-                Genre text,
-                SubGenre text,
-                Pages integer,
-                Publisher text,
-                ReturnDate text
-                )
-            """
+def withdraw_book():
+    while True:
+        title = input("Please Select the title of the book to withdraw")
+        with lib_db:
+            curs.execute(
+                """
+                SELECT * FROM inHouse WHERE Title = :title
+                """,
+                {"title": title},
             )
-        except:
-            pass
-        wcurs.execute(
-            """INSERT INTO withdrawals VALUES (?, ?, ?, ?, ?, ?, ?)""",(*book_info, return_date))
-        with_db.commit()
-        wcurs.execute(
-            """SELECT * FROM withdrawals"""
+            book_info = curs.fetchone()
+            if len(book_info) == 0:
+                print("Book title did not match, please select again")
+            else:
+                break
+
+    with with_db:
+        wcurs.execute("SELECT * FROM withdrawals WHERE Title = :title" , {'title': title})
+
+        if len(wcurs.fetchall()) != 0:
+            date = list(wcurs.fetchone())[-1]
+            print(
+                f"It appears that this book is currently checked out and is due back at {date}",
+                "\nWould you like to select another book or return to main menu?\n",
+                "1) Select another book\n",
+                "2) Return to Main Menu\n",
+            )
+            action = input(
+                "Please select the number corresponding to the action")
+            if action == 1:
+                withdraw_book()
+            main_menu()
+        else:
+            try:
+                wcurs.execute(
+                    """
+                CREATE TABLE withdrawals (
+                    Title text,
+                    Author text,
+                    Genre text,
+                    SubGenre text,
+                    Pages integer,
+                    Publisher text,
+                    ReturnDate text
+                    )
+                """
+                )
+            except:
+                pass
+            wcurs.execute(
+                """INSERT INTO withdrawals VALUES (?, ?, ?, ?, ?, ?, ?)""",
+                (*book_info, return_date),
+            )
+            with_db.commit()
+            wcurs.execute("""SELECT * FROM withdrawals""")
+            s = wcurs.fetchall()
+            print(s)
+            # with_db.commit()
+            return wcurs, with_db
+
+
+def reset_withdrawn_db():
+    with with_db:
+        wcurs.execute("DELETE FROM withdrawals")
+        wcurs.execute("SELECT * FROM withdrawals")
+        proof = wcurs.fetchall()
+    print(proof)
+    main_menu()
+
+
+def search_for_book():
+    print(
+        "\nWhat would you like to search by? :\n",
+        "1) Title\n",
+        "2) Author\n",
+        "3) Genre\n",
+        "4) SubGenre\n",
+        "5) Publisher\n",
+    )
+    response = input("Please select an option by it's corresponding number")
+    search_cats = {
+        "1": "Title",
+        "2": "Author",
+        "3": "Genre",
+        "4": "SubGenre",
+        "5": "Publisher"
+    }
+    cat = search_cats[response]
+    term = input(f'Please input the {cat} name to search: ')
+    curs.execute("SELECT * FROM inHouse WHERE ? = ?", (cat, term))
+    responses = curs.fetchall()
+    if len(responses) == 0:
+        print(
+            "No results were found. Would you like to search again?\n"
         )
-        s = wcurs.fetchall()
-        print(s)
-        # with_db.commit()
-        return wcurs, with_db
+        ans = input('Y/n?')
+        if ans.upper() == 'Y':
+            search_for_book()
+        return main_menu()
+    else:
+        columns=["Title", "Author", "Genre", "SubGenre", "Pages", "Publisher"],
+        df = pd.DataFrame(responses, columns= columns)
+        tab_df(df)
 
-
-# wcurs, with_db = withdraw_book('Fundamentals of Wavelets', '12/12/12')
-
-# print(curs.fetchall())
-# print('\n', wcurs.fetchall())
-
-
-# add_book('yaba', 'dC', 'Fictiona', 'diction', 223, 'wainscott')
-
-
-
-# print(curs.fetchall())
 
 def main_menu():
     print(
         "1) Browse Books\n",
-        "2) Add a Book\n",
-        "3) Withdraw a Book\n",
-        "4) Return all Withdrawn\n",
-        "5) Exit\n",
-        )
-    pick = input('Enter the number of the action you would like to take: ')
-    if pick not in [1,2,3,4,5]:
+        "2) Search Books\n",
+        "3) Add a Book\n",
+        "4) Withdraw a Book\n",
+        "5) Return all Withdrawn\n",
+        "6) Exit\n",
+    )
+    pick = input("Enter the number of the action you would like to take: ")
+    if int(pick) not in [1, 2, 3, 4, 5, 6]:
         print("Selection Not Valid, please pick again!")
         main_menu()
     actions = {
         "1": display_books,
-        "2": add_book,
-        "3": withdraw_book,
-        "4": reset_withdrawn_db,
-        "5": lambda: print("\nGoodbye!"),
+        "2": search_for_book,
+        "3": add_book,
+        "4": withdraw_book,
+        "5": reset_withdrawn_db,
+        "6": lambda: print("\nGoodbye!"),
     }
     pick = str(pick).strip()
     actions[pick]()
 
 
-main_menu()
-
-
 def lib_program():
     print(
-            "Welcome to the Library Management System!\nPlease choose from the following:\n",
+        "Welcome to the Library Management System!\nPlease choose from the following:\n",
     )
     main_menu()
 
 
-
+if __name__ == "__main__":
+    lib_db, curs = initialize_lib_db()
+    with_db, wcurs = initialize_withdrawals()
+    lib_program()
